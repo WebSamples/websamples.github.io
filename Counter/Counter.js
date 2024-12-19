2c24bc13104707fcd2f25bc14226e3fc7dbaa7f9
Skj0nes2 <aric.jones@student.msdmartinsville.org>
2024-12-19 14:11:13 -0500

    Update Counter.js

--- Counter/Counter.js
+++ Counter/Counter.js
@@ -1,1 +1,13 @@
-
+let x = 0;
+const counterDisplay = document.getElementById('counterDisplay');
+function CountUp() {
+  x++;
+  updateCounter();
+}
+function CountDown() {
+  x--;
+  updateCounter();
+}
+function updateCounter() {
+  counterDisplay.textContent = x;
+}
